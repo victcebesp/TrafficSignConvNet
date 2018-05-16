fe953153059ad65dec3d844f84dc477ddd711686
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2018-05-16 16:19:56 +0100

    Refactor: variable extract

--- modelRestorerWithPIL.py
+++ modelRestorerWithPIL.py
@@ -12,10 +12,11 @@ def get_images_paths(path, maxFiles):
         files.append(np.asarray(Image.open(infile)) / 256. + 0.5)
     return files
 
+batch_size = 117
 
-images0, label0 = get_images_paths("data/test/0/*.ppm", 117), [0., 0., 1.]
-images1, label1 = get_images_paths("data/test/1/*.ppm", 117), [0., 1., 0.]
-images2, label2 = get_images_paths("data/test/2/*.ppm", 117), [1., 0., 0.]
+images0, label0 = get_images_paths("data/test/0/*.ppm", batch_size), [0., 0., 1.]
+images1, label1 = get_images_paths("data/test/1/*.ppm", batch_size), [0., 1., 0.]
+images2, label2 = get_images_paths("data/test/2/*.ppm", batch_size), [1., 0., 0.]
 
 for i in range(0, len(images0)):
     images0[i] = np.reshape(images0[i], [80, 80, 1])
@@ -23,7 +24,6 @@ for i in range(0, len(images0)):
     images2[i] = np.reshape(images2[i], [80, 80, 1])
 
 num_classes = 3
-batch_size = 5
 
 # --------------------------------------------------
 #
@@ -43,7 +43,7 @@ with tf.variable_scope('ConvNet'):
     o4 = tf.layers.max_pooling2d(inputs=o3, pool_size=2, strides=2)  # output -> (x,y,z) = (18, 18, 64)
 
     # Change from batch_size * 2 -> batch_size * 3
-    h = tf.layers.dense(inputs=tf.reshape(o4, [117, 18 * 18 * 64]), units=10, activation=tf.nn.relu)
+    h = tf.layers.dense(inputs=tf.reshape(o4, [batch_size, 18 * 18 * 64]), units=10, activation=tf.nn.relu)
     y = tf.layers.dense(inputs=h, units=3, activation=tf.nn.softmax)
 
 saver = tf.train.Saver()
@@ -58,12 +58,12 @@ with tf.Session() as sess:
     output1 = [np.argmax(e) for e in output1]
     output2 = [np.argmax(e) for e in output2]
 
-    confusion_matrix0 = sess.run(tf.confusion_matrix(np.zeros(117), np.array(output0), 3))
-    confusion_matrix1 = sess.run(tf.confusion_matrix(np.zeros(117) + 1, np.array(output1), 3))
-    confusion_matrix2 = sess.run(tf.confusion_matrix(np.zeros(117) + 2, np.array(output2), 3))
+    confusion_matrix0 = sess.run(tf.confusion_matrix(np.zeros(batch_size), np.array(output0), 3))
+    confusion_matrix1 = sess.run(tf.confusion_matrix(np.zeros(batch_size) + 1, np.array(output1), 3))
+    confusion_matrix2 = sess.run(tf.confusion_matrix(np.zeros(batch_size) + 2, np.array(output2), 3))
 
     right_answers = np.diagonal(confusion_matrix0).sum() + \
                     np.diagonal(confusion_matrix1).sum() + \
                     np.diagonal(confusion_matrix2).sum()
 
-    print "Acurracy: " + str(right_answers * 100 / (117 * 3)) + "%"
+    print "Accuracy: " + str(right_answers * 100 / (batch_size * 3)) + "%"
